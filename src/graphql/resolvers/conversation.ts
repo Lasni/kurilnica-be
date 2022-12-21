c07557e6da23a8547d744a8d2217fc7d68374d33
Lasni <grega.lasnibat@gmail.com>
2022-12-21 12:49:32 +0100

    Fixed 'must have same native type' warning via @db.ObjectId, made latestMessageId optional to fix createConversation error where it expected String but got null

--- src/graphql/resolvers/conversation.ts
+++ src/graphql/resolvers/conversation.ts
@@ -13,38 +13,42 @@ const conversationResolvers = {
     ): Promise<{ conversationId: string }> => {
       const { participantIds } = args;
       const { prisma, session } = contextValue;
-      // console.log("participantIds:", participantIds);
       if (!session?.user) {
         throw new GraphQLError("Not authorized");
       }
       const { id: userId } = session.user;
 
       try {
+        console.log("participantIds:", participantIds);
         const conversation = await prisma.conversation.create({
           data: {
             participants: {
               createMany: {
-                data: participantIds.map((id) => ({
+                data: participantIds?.map((id) => ({
                   userId: id,
-                  hasSeenLatestMessage: id === userId, // makes sense that the creator of conversation has this as true
+                  hasSeenLatestMessage: id === userId, // makes sense that the creator of conversation has seen the message
                 })),
               },
             },
           },
-          include: conversationPopulated,
+          include: conversationValidated, // stuff that we want returned on our conversation object
         });
+
+        console.log("conversation: ", conversation);
+
+        // emmit CONVERSATION_CREATED event using pubsub
         return {
           conversationId: conversation.id,
         };
       } catch (error) {
-        throw new GraphQLError("Error creating conversation");
+        throw new GraphQLError(`Error creating conversation: ${error}`);
       }
     },
   },
   // Subscription: {}
 };
 
-export const participantPopulated =
+export const participantValidated =
   Prisma.validator<Prisma.ConversationParticipantInclude>()({
     user: {
       select: {
@@ -54,10 +58,19 @@ export const participantPopulated =
     },
   });
 
-export const conversationPopulated =
+// export const senderValidated = Prisma.validator<Prisma.MessageInclude>()({
+//   sender: {
+//     select: {
+//       id: true,
+//       username: true,
+//     },
+//   },
+// });
+
+export const conversationValidated =
   Prisma.validator<Prisma.ConversationInclude>()({
     participants: {
-      include: participantPopulated,
+      include: participantValidated,
     },
     latestMessage: {
       include: {
