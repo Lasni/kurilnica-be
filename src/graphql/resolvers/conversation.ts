Lasni/kurilnica-be

commit c4156ae9013d0115891f7fd7ee0f169e4e1e2631

Working on adding users via updateConversation. Introduced latestMessage bug.

src/graphql/resolvers/conversation.ts
@@ -260,11 +260,44 @@ const conversationResolvers = {
       context: GraphQLContext
     ): Promise<UpdateConversationMutationResponse> {
       console.log("updateConversation resolver: -> args: ", args);
+      const { prisma, pubsub, session } = context;
+      const { conversationId, participantIds } = args;
+
+      const {
+        user: { id: userId },
+      } = session;
+
       try {
+        const updatedConversation = await prisma.conversation.update({
+          where: {
+            id: conversationId,
+          },
+          data: {
+            participants: {
+              createMany: {
+                data: participantIds.map((id) => ({
+                  userId: id,
+                  hasSeenLatestMessage: false,
+                })),
+              },
+            },
+          },
+          include: conversationPopulated,
+        });
+
+        pubsub.publish(ConversationEnum.CONVERSATION_UPDATED, {
+          conversationUpdated: {
+            conversation: updatedConversation,
+            addedUserIds: participantIds,
+          },
+        });
+
+        // console.log("conversation: ", updatedConversation);
+
         return {
-          conversationId: args.conversationId,
           success: true,
           error: "",
+          conversationId: updatedConversation.id,
         };
       } catch (error: any) {
         console.error("updateConversation resolver error: ", error);
@@ -319,27 +352,36 @@ const conversationResolvers = {
             conversationUpdated: {
               conversation: { participants },
               removedUserIds,
+              addedUserIds,
             },
           } = payload;
 
-          const userIsParticipant = userIsConversationParticipant(
-            participants,
-            userId
-          );
+          /**
+           * Removing a user
+           */
+          const userIsBeingRemoved =
+            typeof removedUserIds !== "undefined" && removedUserIds.length > 0;
+          // && Boolean(removedUserIds?.find((id) => id === userId));
+
+          /**
+           * Adding a user
+           */
+          const usersAreBeingAdded =
+            typeof addedUserIds !== "undefined" && addedUserIds.length > 0;
+
+          // const userIsParticipant = userIsConversationParticipant(
+          //   participants,
+          //   userId
+          // );
 
           // const userSentLatestMessage =
           //   payload.conversationUpdated.conversation.latestMessage?.senderId ===
           //   userId;
 
-          const userIsbeingRemoved =
-            typeof removedUserIds !== "undefined" &&
-            removedUserIds.length > 0 &&
-            Boolean(removedUserIds?.find((id) => id === userId));
-
           return (
-            userIsParticipant ||
+            // userIsParticipant ||
             // userSentLatestMessage ||
-            userIsbeingRemoved
+            userIsBeingRemoved || usersAreBeingAdded
           );
         }
       ),

src/graphql/typeDefs/conversation.ts
@@ -79,6 +79,7 @@ const conversationTypeDefs = `#graphql
   type ConversationUpdatedSubscriptionPayload {
     conversation: Conversation
     removedUserIds: [String]
+    addedUserIds: [String]
   }
 
   type Subscription {

src/graphql/resolvers/message.ts
@@ -160,6 +160,7 @@ const messageResolvers = {
           conversationUpdated: {
             conversation: updatedConversation,
             removedUserIds: [],
+            // addedUserIds: []
           },
         });
         return {

src/interfaces/graphqlInterfaces.ts
@@ -130,7 +130,8 @@ export interface ConversationCreatedSubscriptionPayload {
 export interface ConversationUpdatedSubscriptionPayload {
   conversationUpdated: {
     conversation: ConversationPopulated;
-    removedUserIds: Array<string>;
+    removedUserIds?: Array<string>;
+    addedUserIds?: Array<string>;
   };
 }
 

commit e980c7b3cb105fa859015f63d39f5ddd08d3165b

Created test-branch02 from 1a492086cdb64557d289a3ba44b602853de2b1ee Working on figuring out how bug was introduced in c4156ae9013d0115891f7fd7ee0f169e4e1e2631

src/graphql/resolvers/conversation.ts
@@ -260,6 +260,15 @@ const conversationResolvers = {
       context: GraphQLContext
     ): Promise<UpdateConversationMutationResponse> {
       console.log("updateConversation resolver: -> args: ", args);
+
+      //! Created test-branch02 where new message updates to conversationItems still works. Will work from here
+      const { prisma, pubsub, session } = context;
+      const { conversationId, participantIds } = args;
+
+      const {
+        user: { id: userId },
+      } = session;
+
       try {
         return {
           conversationId: args.conversationId,